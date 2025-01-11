86546046e9f6d8397bd564bcc935ebf16cf88d48
nomeera <abdullahnomeer@gmail.com>
2025-01-12 01:39:03 +0200

    nomeer_ackerman_robot

--- README.md
+++ README.md
@@ -1,1 +1,91 @@
 # nomeer_ackerman_robot
+
+## Overview
+The **Ackerman Robot** is a ROS 2-based robot designed with an Ackerman steering mechanism. This project includes two main packages:
+
+1. **robot_description** - Handles the robot’s physical description and integration with Gazebo and Rviz.
+2. **teleop_twist_keyboard** - Enables manual control of the robot using keyboard inputs.
+
+---
+
+## Package Details
+
+### 1. **robot_description**
+This package contains all the necessary files to describe the Ackerman Robot and manage its simulation in Gazebo.
+
+#### Directory Structure:
+- **config/**: Contains configuration files for Gazebo bridge parameters.
+- **launch/**: Includes launch files to start the Gazebo world and robot description.
+  - Example command to launch:
+    ```bash
+    ros2 launch robot_description ackerman_robot.launch.py
+    ```
+- **models/**: Contains the Ackerman Robot’s SDF file.
+- **worlds/**: Includes custom Gazebo world files, such as `warehouse.sdf`.
+- **rviz/**: Includes the Rviz2 configuration file.
+- **CMakeLists.txt** and **package.xml**: Define package dependencies and build instructions.
+
+### 2. **teleop_twist_keyboard**
+This package allows users to control the Ackerman Robot via keyboard input. It provides:
+- A Python-based script to capture keyboard inputs and send velocity commands to the robot via `/cmd_vel`.
+
+---
+
+## Getting Started
+
+### Prerequisites
+Ensure you have the following installed:
+- ROS 2 (Humble)
+- Gazebo Harmonic
+
+### Installation
+1. Clone the repository to your workspace:
+   ```bash
+   git clone https://github.com/nomeera/nomeer_ackerman_robot.git
+   ```
+2. Build the workspace:
+   ```bash
+   cd <workspace>
+   colcon build --symlink-install --allow-overriding teleop_twist_keyboard
+   ```
+3. Source the workspace:
+   ```bash
+   source install/setup.bash
+   ```
+
+---
+
+## Usage
+
+### Launching the Robot in Gazebo
+Run the following command to launch the robot in the warehouse simulation:
+```bash
+ros2 launch robot_description ackerman_robot.launch.py
+```
+
+### Controlling the Robot
+1. Open a new terminal and source the workspace.
+2. Run the teleoperation script:
+   ```bash
+   ros2 run teleop_twist_keyboard teleop_twist_keyboard
+   ```
+
+Follow the on-screen instructions to control the robot with your keyboard.
+
+---
+
+## Robot Image
+![](/docs/Screenshot%20from%202025-01-12%2001-30-09.png)
+---
+
+## Future Plans
+- Implement autonomous navigation for the Ackerman Robot.
+- Develop advanced motion planning algorithms tailored for the Ackerman steering mechanism.
+
+---
+
+## License
+This project is licensed under the [Apache License](src/robot_description/LICENSE).
+
+---
+
---
+++ src/robot_description/launch/robot.launch.py
@@ -0,0 +1,99 @@
+import os  # Provides utilities for handling file paths and interacting with the file system.
+from ament_index_python.packages import (
+    get_package_share_directory,
+)  # Retrieves the share directory of a ROS 2 package.
+import xml.etree.ElementTree as ET
+
+# these modules define the structure and actions of the launch file.
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import PathJoinSubstitution
+
+# Used to define and launch ROS 2 nodes
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+
+    # setup paths
+    pkg_ros_gz_sim = get_package_share_directory("ros_gz_sim")
+    pkg_project_description = get_package_share_directory("robot_description")
+
+    # load sdf file
+    sdf_file = os.path.join(
+        pkg_project_description, "models", "nomeer_robot", "robot.sdf"
+    )
+    with open(sdf_file, "r") as infp:
+        robot_desc = infp.read()
+          # Reads the content of the SDF file into a string for use in other components.
+
+    # launch gazebo
+    gz_sim = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                pkg_ros_gz_sim, "launch", "gz_sim.launch.py"
+            )  # Includes another launch file to start the Gazebo simulation.
+        ),
+        launch_arguments={
+            "gz_args": PathJoinSubstitution(
+                [pkg_project_description, "worlds", "empty.sdf"]
+            )
+        }.items(),
+    )
+
+    # joint state publisher to publish joint state
+    joint_state_publisher = Node(
+        package="joint_state_publisher",
+        executable="joint_state_publisher",
+        name="joint_state_publisher",
+        output="both",
+        parameters=[
+            {"use_sim_time": True},
+        ],
+    )
+
+    # Takes the description and joint angles as inputs and publishes the 3D poses of the robot links
+    robot_state_publisher = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        name="robot_state_publisher",
+        output="both",
+        parameters=[
+            {"use_sim_time": True},
+            {"robot_description": robot_desc},
+        ],
+    )
+
+    # Bridge ROS topics and Gazebo messages for establishing communication
+    bridge = Node(
+        package="ros_gz_bridge",
+        executable="parameter_bridge",
+        parameters=[
+            {
+                "config_file": os.path.join(
+                    pkg_project_description, "config", "ros_gz_bridge.yaml"
+                ),
+                "qos_overrides./tf_static.publisher.durability": "transient_local",  # Overrides the QoS (Quality of Service) settings for the /tf_static topic to make it durable (e.g., for transformations).
+            }
+        ],
+        output="screen",
+    )
+
+    # rviz2
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        output="screen",
+        name="sim_rviz2",
+        arguments=['-d', 'src/robot_description/rviz/rviz.rviz']    
+        )
+
+    return LaunchDescription([
+        gz_sim,
+        robot_state_publisher,
+        joint_state_publisher,
+        bridge, 
+        rviz_node
+        ])
